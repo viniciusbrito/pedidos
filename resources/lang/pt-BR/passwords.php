0358cfecfd7eca290b21c9acc612c6afc95f7bbe
viniciusbrito <vinicius.fernandes.brito@gmail.com>
2015-05-07 16:05:54 -0300

    First Commit

---
+++ resources/lang/pt-BR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "As senhas devem ter pelo menos seis caracteres e combinar com a confirma&ccedil;&atilde;o.",
+	"user" => "N&atilde;o podemos encontrar um usu&aacute;rio com esse endere&ccedil;o de e-mail.",
+	"token" => "Este token de redefini&ccedil;&atilde;o de senha &eacute; inv&aacute;lida.",
+	"sent" => "Enviamos por e-mail o seu link de redefini&ccedil;&atilde;o de senha!",
+	"reset" => "A sua senha foi redefinida!",
+
+];
