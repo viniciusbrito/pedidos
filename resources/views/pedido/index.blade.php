fc8fe0dfe38bf74e0ea3e6965d1eab6c2df6ff44
viniciusbrito <vinicius.fernandes.brito@gmail.com>
2015-05-08 23:33:07 -0300

    Criando Rotas/Controller/View para Produto

---
+++ resources/views/pedido/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Web - Pedidos</title>
+</head>
+<body>
+    <h1>Pedidos</h1>
+</body>
+</html>
---
+++ resources/views/produto/index.blade.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Produtos</title>
+</head>
+<body>
+    <h1>Produtos</h1>
+</body>
+</html>
